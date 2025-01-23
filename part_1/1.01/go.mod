cd02c7aa948597d8a2307e4773ef28d6b763ca3c
szymonsitko <szymon.sitko@onet.eu>
2025-01-23 16:53:08 +0100

    Completed all excersises for section 3

--- part_1/1.01/go.mod
+++ part_1/1.01/go.mod
@@ -2,4 +2,7 @@ module github.com/szymonsitko/hash-generator
 
 go 1.23.5
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/google/uuid v1.6.0
+	github.com/gorilla/mux v1.8.1
+)
--- part_1/1.01/main.go
+++ part_1/1.01/main.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/gorilla/mux"
 )
 
+var PORT = os.Getenv("PORT")
+
+var (
+	currentTime string
+	hash        string
+)
+
+func http_server() {
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+	fmt.Printf(">>> Starting web server at port: %s\n", PORT)
+	r := mux.NewRouter()
+
+	r.NotFoundHandler = http.HandlerFunc(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[DEBUG] Incoming request: %s", r.RequestURI)
+	}))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, hash)
+	})
+
+	http.ListenAndServe(":"+PORT, r)
+}
+
 func main() {
+	go http_server()
+
 	for {
-		currentTime := time.Now().UTC().Format(time.RFC3339)
-		fmt.Printf("%s: %s\n", currentTime, uuid.New())
+		currentTime = time.Now().UTC().Format(time.RFC3339)
+		hash = fmt.Sprintf("%s: %s\n", currentTime, uuid.New())
+		fmt.Print(hash)
 		time.Sleep(5 * time.Second)
 	}
 }
